dbcb4de9abc2eb813e2a828d44b5957671154aac
Yinyi Xue <xueyin@bc.edu>
2026-01-23 01:04:55 +0000

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An applied analytics project analyzing the relationship between telecom subscriptions, regional market structure (circles), pricing, and service quality.',
+    author='TeamGogoLeoTaki',
+    license='MIT',
+)
